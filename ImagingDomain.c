dcbd8d1f07b8c944346edc4e58395b5dc0527889
Coltoncx245 <30328876+Coltoncx245@users.noreply.github.com>
2023-06-22 13:13:41 -0500

    imaging domain + main and makefile

---
+++ ImagingDomain.c
@@ -0,0 +1,48 @@
+#include "ImagingDomain.h"
+
+
+void GenerateImagingDomain(float imaging_domain[MAX_SIZE][3])
+{
+
+    int index = 0;
+    
+
+    for(float height=0; height < SPHERE_RADIUS * 2; height += RESOLUTION)
+    {
+        float circle_radius = sqrtf(powf(SPHERE_RADIUS, 2) - powf((SPHERE_RADIUS - height), 2));
+
+        for (float angle = 0; angle < 2 * M_PI; angle += ANGULAR_RESOLUTION)
+        {
+
+            for (float r = 0; r < circle_radius; r += RESOLUTION)
+            {
+                
+                imaging_domain[index][0] = r * cosf(angle);
+                imaging_domain[index][1] = r * sinf(angle);
+                imaging_domain[index][2] = height;
+                
+                index++;
+
+            }
+
+        }
+
+    }
+
+
+// If WRITE_TO_FILE defined in .h will write results to output.csv
+#ifdef WRITE_TO_FILE
+    FILE* fp;
+    fp = fopen("output.csv", "w+");
+    for (int i=0; i < index; i++)
+    {
+        fprintf(fp, "%f,", imaging_domain[i][0]);
+        fprintf(fp, "%f,", imaging_domain[i][1]);
+        fprintf(fp, "%f\n", imaging_domain[i][2]);
+    }
+
+    fclose(fp);
+#endif
+
+}
+
---
+++ main.c
@@ -0,0 +1,16 @@
+#include "ImagingDomain.h"
+
+float ID[MAX_SIZE][3];
+
+int main()
+{
+
+    GenerateImagingDomain(ID);
+
+    // print first 20 for testing
+    for (int i=0;i<20;i++)
+    {
+       printf("%f\n", ID[i][0]);
+    }
+    return 0;
+}
