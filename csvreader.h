610f6d6b84bde0b5183c915e98c52389ec99b025
zenOfBass <108999864+zenOfBass@users.noreply.github.com>
2023-06-27 15:26:11 -0500

    fixes

--- csvreader.h
+++ csvreader.h
@@ -1,19 +1,10 @@
-/*
-csvreader.c
-Revised 6/22/2023 
-Nathan Wiley - nwiley @uco.edu
-*/
-
-//////////////////////////////////////////////////////
-// Imports
+#ifndef CSV_READER
+#define CSV_READER
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <complex.h>
 #include <string.h>
-
-//////////////////////////////////////////////////////
-// Define constants
 
 #define MAX_ROWS 300            // Maximun rows for 2D channel names, frequencies, and 2D antenna locations arrays
 #define MAX_COLS 3              // Maximun comlumns for 2D channel names and 2D antenna locations arrays
@@ -21,6 +12,9 @@ Nathan Wiley - nwiley @uco.edu
 #define MAX_COMPLEX_NUMBERS 500 // Maximun comlumns for 2D complex number array (IQ data)
 #define BUFFER_SIZE 4096        // Maximum buffer size for line when reading complex number CSV data file
 
-// Function definitions
-void chan_freq_antloc_reader(const char *chan, const char *freq, const char *antloc);
-void iq_reader(const char *iq);
+void readIntArray(const char *fileName, int intArray[MAX_ROWS][MAX_COLS], int *numRows);
+void readDoubleArray(const char *fileName, double doubleArray[MAX_ROWS], int *numRows);
+void readFloatArray(const char *fileName, float floatArray[MAX_ROWS][MAX_COLS], int *numRows);
+void readComplexArray(const char *fileName, long double complex **complexArray, int *numRows, int numComplexNumbers[]);
+
+#endif
