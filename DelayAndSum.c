d7ee386122ac34ec9fa09b105912a5be25d538f8
Nathan Wiley <nwiley@uco.edu>
2023-07-18 13:37:05 -0500

    fudge

--- DelayAndSum.c
+++ DelayAndSum.c
@@ -17,13 +17,13 @@ double timeDelay(float RX_x, float RX_y, float RX_z, double ID_x, double ID_y, d
 
 void delayAndSum(
                 int intArray[MAX_ROWS][MAX_COLS],
-                double doubleArray[MAX_ROWS],
+                double frequencies[MAX_ROWS],
                 float floatArray[MAX_ROWS][MAX_COLS],
                 long double complex **complexArray,
                 double imagingDomain[MAX_SIZE][3],
                 int imagingDomainSize)
 {
-     for (int r = 0; r < imagingDomainSize; r++)
+    for (int r = 0; r < imagingDomainSize; r++)
     {
         for (int m = 0; m < NUM_OF_CHANNELS; m++)
         {
@@ -40,8 +40,12 @@ void delayAndSum(
                 float RX_y = (floatArray[RXAnt])[1];
                 float RX_z = (floatArray[RXAnt])[2];
 
+                // printf("Complex Value: %.20Lf + %.20Lfi\n", creall(IQData), cimagl(IQData));
+
                 //printf("%.30e\n", creall(timeDelay(RX_x, RX_y, RX_z, ID_x, ID_y, ID_z)*I*2*3.141592));
-                imagingDomain[r][2] += fabsl(100000000000 * creal(IQData * I * (timeDelay(RX_x, RX_y, RX_z, ID_x, ID_y, ID_z)*2*PI)));
+                double intensity = creal(IQData * cexp(-1 * I * 2 * M_PI * timeDelay(RX_x, RX_y, RX_z, ID_x, ID_y, ID_z) * frequencies[f]));
+                printf("%.lf\n", intensity);
+                imagingDomain[r][2] += intensity;
                 //printf("%.30e\n", imagingDomain[r][2]);
             }
         }
