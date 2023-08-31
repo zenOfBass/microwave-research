13dff4a53e5a69175ecaf8ebe0cc340685c3a0fc
Nathan Wiley <nwiley@uco.edu>
2023-08-31 12:24:58 -0500

    Make it so.

--- csvReader.c
+++ csvReader.c
@@ -1,118 +1,129 @@
-#include "csvReader.h"
+/*
+CSVReader.c
+Revised 8/2/2023
+Nathan Wiley - nwiley@uco.edu
+*/
 
-#define MAX_ROWS 300            // Maximun rows for 2D channel names, frequencies, and 2D antenna locations arrays
-#define MAX_COLS 3              // Maximun comlumns for 2D channel names and 2D antenna locations arrays
-#define MAX_ROWS_COMPLEX 500    // Maximun rows for 2D complex number array (IQ data)
-#define MAX_COMPLEX_NUMBERS 500 // Maximun comlumns for 2D complex number array (IQ data)
-#define BUFFER_SIZE 4096        // Maximum buffer size for line when reading complex number CSV data file
+#include "CSVReader.h"
 
-void readIntArray(const char *fileName, int intArray[MAX_ROWS][MAX_COLS], int *numRows)
+void readIntArray(const char *fileName, int intArray[MAX_ROWS][3], int *numRows)
 {
-    FILE *file = fopen(fileName, "r");
-    if (file == NULL)
+    FILE *file = fopen(fileName, "r"); // Open file
+    if (file == NULL)                  // If the file isn't found...
     {
-        printf("Error opening file (%s).\n", fileName);
+        printf("Error opening file (%s).\n", fileName); // Print error to terminal
         *numRows = 0;
-        return;
+        exit(1); // Exit
     }
 
+    // Read file
     int rows = 0, cols = 0;
-    while (fscanf(file, "%d,%d,", &intArray[rows][cols], &intArray[rows][cols + 1]) == 2)
+    while (fscanf(file, "%d,%d", &intArray[rows][cols], &intArray[rows][cols + 1]) == 2)
     {
+        /*  Read two integers separated by a comma
+            Store the first integer in intArray[rows1][cols1]
+            Store the second integer in intArray[rows1][cols1 + 1] */
         rows++;
-        if (rows >= MAX_ROWS)
+        if (rows >= MAX_ROWS) // If the number of rows read exceeds what is allowed for the array...
         {
-            printf("Maximum number of rows exceeded (%s).\n", fileName);
-            break;
+            printf("Maximum number of rows exceeded (%s).\n", fileName); // Print error to terminal
+            break;                                                       // Break loop from error
         }
     }
-    fclose(file);
+    fclose(file); // Close file
 
     *numRows = rows;
 }
 
 void readDoubleArray(const char *fileName, double doubleArray[MAX_ROWS], int *numRows)
 {
-    FILE *file = fopen(fileName, "r");
-    if (file == NULL)
+    FILE *file = fopen(fileName, "r"); // Open file
+    if (file == NULL)                  // If the file isn't found...
     {
-        printf("Error opening file (%s).\n", fileName);
+        printf("Error opening file (%s).\n", fileName); // Print error to terminal
         *numRows = 0;
-        return;
+        exit(1); // Exit
     }
 
+    // Read file
     int rows = 0;
     while (fscanf(file, "%lf,", &doubleArray[rows]) == 1)
     {
+        /*  Read a double value followed by a comma
+            Store the double value in doubleArray[rows2] */
         rows++;
-        if (rows >= MAX_ROWS)
+        if (rows >= MAX_ROWS) // If the number of rows read exceeds what is allowed for the array...
         {
-            printf("Maximum number of rows exceeded (%s).\n", fileName);
-            break;
+            printf("Maximum number of rows exceeded (%s).\n", fileName); // Print error to terminal
+            break;                                                       // Break loop from error
         }
     }
-    fclose(file);
+    fclose(file); // Close file
 
     *numRows = rows;
 }
 
-void readFloatArray(const char *fileName, float floatArray[MAX_ROWS][MAX_COLS], int *numRows)
+void readFloatArray(const char *fileName, float floatArray[MAX_ROWS][3], int *numRows)
 {
-    FILE *file = fopen(fileName, "r");
-    if (file == NULL)
+    FILE *file = fopen(fileName, "r"); // Open file
+    if (file == NULL)                  // If the file isn't found...
     {
-        printf("Error opening file (%s).\n", fileName);
+        printf("Error opening file (%s).\n", fileName); // Print error to terminal
         *numRows = 0;
-        return;
+        exit(1); // Exit
     }
 
+    // Read file
     int rows = 0, cols = 0;
     while (fscanf(file, "%f,%f,%f,", &floatArray[rows][cols], &floatArray[rows][cols + 1], &floatArray[rows][cols + 2]) == 3)
     {
+        /*  Read three float values separated by commas from file3
+            Store the float values in floatArray[rows3][0], floatArray[rows3][1], and floatArray[rows3][2] */
         rows++;
-        if (rows >= MAX_ROWS)
+        if (rows >= MAX_ROWS) // If the number of rows read exceeds what is allowed for the array...
         {
-            printf("Maximum number of rows exceeded (%s).\n", fileName);
-            break;
+            printf("Maximum number of rows exceeded (%s).\n", fileName); // Print error to terminal
+            break;                                                       // Break loop from error
         }
     }
-    fclose(file);
+    fclose(file); // Close file
 
     *numRows = rows;
 }
 
 void readComplexArray(const char *fileName, long double complex **complexArray, int *numRows, int numComplexNumbers[])
 {
-    FILE *file = fopen(fileName, "r");
-    if (file == NULL)
+    FILE *file = fopen(fileName, "r"); // Open file
+    if (file == NULL)                  // If the file isn't found...
     {
-        printf("Error opening IQ CSV (%s).\n", fileName);
+        printf("Error opening IQ CSV (%s).\n", fileName); // Print error to terminal
         *numRows = 0;
-        return;
+        exit(1); // Exit
     }
 
+    // Read file
     char line[BUFFER_SIZE];
     int row = 0;
-    while (fgets(line, sizeof(line), file))
+    while (fgets(line, sizeof(line), file)) // Read a line from 'file' and store it in 'line'
     {
         char *token;
         char *rest = line;
-        while ((token = strtok_r(rest, ",", &rest)))
+        while ((token = strtok_r(rest, ",", &rest))) // Split 'line' into tokens using the delimiter ',' and store each 'token'
         {
-            char *endptr;
-            long double real = strtold(token, &endptr);
-            long double imag = strtold(endptr, &endptr);
-            complexArray[row][numComplexNumbers[row]] = real + imag * I;
-            numComplexNumbers[row]++;
+            char *endptr;                                                // Extract the real and imaginary parts from 'token'
+            long double real = strtold(token, &endptr);                  // Convert the token to a real number
+            long double imag = strtold(endptr, &endptr);                 // Convert the remaining string to an imaginary number
+            complexArray[row][numComplexNumbers[row]] = real + imag * I; // Store complex number in array by adding and multiplying by 'i'
+            numComplexNumbers[row]++;                                    // Increment the count of complex numbers for the current row
         }
         row++;
-        if (row >= MAX_ROWS_COMPLEX)
+        if (row >= MAX_ROWS_COMPLEX) // If the number of rows read exceeds what is allowed for the array...
         {
-            printf("Maximum number of rows exceeded (%s).\n", fileName);
-            break;
+            printf("Maximum number of rows exceeded (%s).\n", fileName); // Print error to terminal
+            break;                                                       // Break loop from error
         }
     }
-    fclose(file);
-
+    fclose(file); // Close file
+    
     *numRows = row;
 }
--- csvReader.h
+++ csvReader.h
@@ -1,3 +1,9 @@
+/*
+CSVReader.h
+Revised 8/3/2023
+Nathan Wiley - nwiley@uco.edu
+*/
+
 #ifndef CSV_READER
 #define CSV_READER
 
@@ -5,16 +11,15 @@
 #include <stdlib.h>
 #include <complex.h>
 #include <string.h>
+#include "Config.h"
 
-#define MAX_ROWS 300            // Maximun rows for 2D channel names, frequencies, and 2D antenna locations arrays
-#define MAX_COLS 3              // Maximun comlumns for 2D channel names and 2D antenna locations arrays
 #define MAX_ROWS_COMPLEX 500    // Maximun rows for 2D complex number array (IQ data)
 #define MAX_COMPLEX_NUMBERS 500 // Maximun comlumns for 2D complex number array (IQ data)
-#define BUFFER_SIZE 4096        // Maximum buffer size for line when reading complex number CSV data file
+#define BUFFER_SIZE 6000        // Maximum buffer size for line when reading complex number CSV data file
 
-void readIntArray(const char *fileName, int intArray[MAX_ROWS][MAX_COLS], int *numRows);
+void readIntArray(const char *fileName, int intArray[MAX_ROWS][3], int *numRows);
 void readDoubleArray(const char *fileName, double doubleArray[MAX_ROWS], int *numRows);
-void readFloatArray(const char *fileName, float floatArray[MAX_ROWS][MAX_COLS], int *numRows);
+void readFloatArray(const char *fileName, float floatArray[MAX_ROWS][3], int *numRows);
 void readComplexArray(const char *fileName, long double complex **complexArray, int *numRows, int numComplexNumbers[]);
 
 #endif
